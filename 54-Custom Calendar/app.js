f48413980d1e445094089a713334502c5da11df0
Ilyes HAMDI <ilyesilyeshamdi10@gmail.com>
2025-07-05 19:09:28 +0100

    "new files"
    "new files"

---
+++ 54-Custom Calendar/app.js
@@ -0,0 +1,10 @@
+function date(){
+    let mounth=document.querySelector(".mounth")
+    console.log(mounth)
+    let x=new Date;
+    let m=Date.getMonth();
+    let y=Date.getFullYear();
+    mounth;
+}
+
+document.addEventListener("load",date());
